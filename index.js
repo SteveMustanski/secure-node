cfdbe03e96c2e2059f831aec5acc13ce12b3e34b
SteveMustanski <steve.mustanski@gmail.com>
2019-06-08 13:40:56 -0400

    updated packages with issues

--- index.js
+++ index.js
@@ -8,9 +8,7 @@ const PORT = 3000;
 
 // mongoose connection
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/CRMdb', {
-    useMongoClient: true
-});
+mongoose.connect('mongodb://localhost/CRMdb');
 
 // bodyparser setup
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -22,9 +20,9 @@ routes(app);
 app.use(express.static('public'));
 
 app.get('/', (req, res) =>
-    res.send(`Node and express server is running on port ${PORT}`)
+  res.send(`Node and express server is running on port ${PORT}`)
 );
 
 app.listen(PORT, () =>
-    console.log(`your server is running on port ${PORT}`)
+  console.log(`your server is running on port ${PORT}`)
 );
