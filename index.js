0113ab897e1d218233caf891a21c72d89c20e5a6
SteveMustanski <steve.mustanski@gmail.com>
2019-06-13 12:33:52 -0400

    added express rate limit

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
 import routes from './src/routes/crmRoutes';
 
 const app = express();
@@ -9,6 +10,15 @@ const PORT = 3000;
 
 // config for helmet
 app.use(helmet());
+
+// set up for rate limit
+
+const limiter = new RateLimit({
+  windowMs: 15 * 60 * 1000, // equals 15 minutes
+  max: 100, // limits the number of requests from a single IP to 100
+  delayMs: 0 // turn off delays
+
+})
 
 // mongoose connection
 mongoose.Promise = global.Promise;
